eb417ea3024f44bd95c7f3446d3312ad2fe2e531
apprajapati9 <apprajapati@hotmail.com>
2021-02-15 21:50:25 -0800

    Basic UI and empty activity for showcase of features

--- app/src/main/java/com/example/apprajapati/databinding/MainActivity.kt
+++ app/src/main/java/com/example/apprajapati/databinding/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.apprajapati.databinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+object TestData{
+    const val temp = 38
+    const val conditions ="Sunny"
+    const val windchill = 6
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        findViewById<Button>(R.id.simplebind).setOnClickListener{
+            startActivity(Intent(this, SimpleDataBindingExample::class.java))
+        }
+
+        findViewById<Button>(R.id.obsfldbind).setOnClickListener{
+            startActivity(Intent(this, ObservableFields::class.java))
+        }
+
+        findViewById<Button>(R.id.viewmodeldbind).setOnClickListener{
+            startActivity(Intent(this, ViewModelExample::class.java))
+        }
+
+        findViewById<Button>(R.id.btnTwoWayBind).setOnClickListener{
+            startActivity(Intent(this, TwoWayDataBinding::class.java))
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/apprajapati/databinding/TwoWayDataBinding.kt
@@ -0,0 +1,12 @@
+package com.example.apprajapati.databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TwoWayDataBinding: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
